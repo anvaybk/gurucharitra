e9fa72e7fb66ac6dd92c45c1e0178b2eb6142bfc
Anvay Kulkarni <48757644+anvay-vignet@users.noreply.github.com>
2024-07-23 00:06:30 +0530

    Add files via upload
    Working website is committed

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// script.js
+
+function startReading() {
+    window.location.href = 'chapters.html';
+}
+
+function goToChapter(chapterUrl) {
+    window.location.href = chapterUrl;
+}
+
+function goToHome() {
+   // window.location.href = 'home.html';
+   // window.location.href = '../index.html';
+	  window.location.href = '../chapters.html';
+}
+
+function goToHomePage() {
+     window.location.href = 'index.html';
+}
