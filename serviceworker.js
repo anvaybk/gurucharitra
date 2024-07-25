3244a275b0881fafdeb8a420c6e00b7eaa73f9bd
Anvay Kulkarni <48757644+anvaybk@users.noreply.github.com>
2024-07-26 01:23:30 +0530

    Update serviceworker.js
    Optimized version

--- serviceworker.js
+++ serviceworker.js
@@ -10,7 +10,7 @@ self.addEventListener("install", function (e) {
         "/index.html", // Cache other important resources like main HTML files
         "/css/styles.css", // Example: Cache CSS files
         "/js/script.js", // Example: Cache JavaScript files
-         "/images/shree_gurucharitra_saramrut.jpg",
+        "/images/shree_gurucharitra_saramrut.jpg",
         "/images/hd-datta_photo1.jpg" // Example: Cache images
       ]);
     })
@@ -40,46 +40,8 @@ self.addEventListener("fetch", function (event) {
 
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      // Return response from cache if available
       if (response) {
         console.log("[Service Worker] Found in Cache:", event.request.url);
-        return response;
-      }
-
-      // Fetch from network if not cached
-      return fetch(event.request).then(function (response) {
-        // Clone the response to cache it
-        var responseClone = response.clone();
-
-        caches.open(staticCacheName).then(function (cache) {
-          cache.put(event.request, responseClone);
-        });
-
-        return response;
-      }).catch(function () {
-        // Handle errors fetching from cache or network
-        console.error("[Service Worker] Error fetching:", event.request.url);
-      });
-    })
-  );
-});
-
-self.addEventListener("message", function (event) {
-  if (event.data.action === "skipWaiting") {
-    self.skipWaiting();
-  }
-});
-
-// Track how the PWA was launched (from cache or network)
-self.addEventListener("fetch", function (event) {
-  console.log("[Service Worker] Fetching:", event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        console.log("[Service Worker] Found in Cache:", event.request.url);
-
-        // Serve from cache if available
         return response;
       }
 
@@ -96,14 +58,11 @@ self.addEventListener("fetch", function (event) {
         return response;
       }).catch(function () {
         console.error("[Service Worker] Error fetching:", event.request.url);
-
-        // Handle errors fetching from cache or network
       });
     })
   );
 });
 
-// Automatically update when there are changes in files
 self.addEventListener("message", function (event) {
   if (event.data.action === "skipWaiting") {
     self.skipWaiting();
