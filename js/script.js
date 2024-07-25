15f077aaf47f2fe51230ac3c0ba1547e715408c9
Anvay Kulkarni <48757644+anvaybk@users.noreply.github.com>
2024-07-26 02:01:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,32 +17,3 @@ function goToHome() {
 function goToHomePage() {
      window.location.href = 'index.html';
 }
-
-// Handle the Install Prompt: Optionally, you can handle the install prompt with a custom event listener for a better user experience.
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the default prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Show your custom install button
-  document.getElementById('install-button').style.display = 'block';
-});
-
-document.getElementById('install-button').addEventListener('click', () => {
-  // Hide the custom install button
-  document.getElementById('install-button').style.display = 'none';
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((result) => {
-    if (result.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-    deferredPrompt = null;
-  });
-});
