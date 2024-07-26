f8e51f4d88161797c3e29cfbecb8f2fbc5b25b99
Anvay Kulkarni <48757644+anvaybk@users.noreply.github.com>
2024-07-26 18:45:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceworker.js')
+    navigator.serviceWorker.register('serviceworker.js')
       .then((registration) => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
