00a9f31eba2461a8cfdb5c1798cdf52450ea7949
Anvay Kulkarni <48757644+anvaybk@users.noreply.github.com>
2024-07-26 16:33:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,51 @@
 // script.js
+// Install the application via button
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/serviceworker.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch((error) => {
+        console.error('ServiceWorker registration failed: ', error);
+      });
+  });
+}
 
+// Handle the installation prompt
+let deferredPrompt;
+const installBtn = document.getElementById('install-btn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = 'block';
+});
+
+installBtn.addEventListener('click', () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((result) => {
+      if (result.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  }
+});
+
+// Hide the install button if the app is already installed
+window.addEventListener('appinstalled', (evt) => {
+  installBtn.style.display = 'none';
+  alert('App is already installed');
+});
+
+
+
+// Functions
 function startReading() {
     window.location.href = 'chapters.html';
 }
