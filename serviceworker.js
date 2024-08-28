b05109f388e653184fd6179b25cf8c7312f7306c
Anvay Kulkarni <48757644+anvaybk@users.noreply.github.com>
2024-08-28 15:41:52 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -117,7 +117,7 @@ self.addEventListener('fetch', event => {
           const responseClone = networkResponse.clone();
           caches.open(staticCacheName).then((cache) => {
             cache.put(event.request, responseClone).catch((error) => {
-              console.error("[Service Worker] Failed to cache response:", error);
+          //    console.error("[Service Worker] Failed to cache response:", error);
             });
           }).catch((error) => {
             console.error("[Service Worker] Failed to open cache:", error);
@@ -140,5 +140,85 @@ self.addEventListener('fetch', event => {
 self.addEventListener("message", (event) => {
   if (event.data.action === "skipWaiting") {
     self.skipWaiting();
+  }
+});
+
+// Adding A Push Listener to our Service Worker
+self.addEventListener('push', (event) => {
+  event.waitUntil(
+    self.registration.showNotification('Notification Title', {
+      body: 'Notification Body Text',
+      icon: 'custom-notification-icon.png',
+    });
+  );
+});
+
+// Handling Notification Clicks
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close(); 
+    var fullPath = self.location.origin + event.notification.data.path; 
+    clients.openWindow(fullPath); 
+});
+
+// Implement Background Sync
+// Check to make sure Sync is supported.
+if ('serviceWorker' in navigator && 'SyncManager' in window) {
+
+  // Get our service worker registration.
+  const registration = await navigator.serviceWorker.registration;
+
+  try {
+    // This is where we request our sync. 
+    // We give it a "tag" to allow for differing sync behavior.
+    await registration.sync.register('database-sync');
+
+  } catch {
+    console.log("Background Sync failed.")
+  }
+}
+
+// Add an event listener for the `sync` event in your service worker.
+self.addEventListener('sync', event => {
+
+  // Check for correct tag on the sync event.
+  if (event.tag === 'database-sync') {
+
+    // Execute the desired behavior with waitUntil().
+    event.waitUntil(
+
+      // This is just a hypothetical function for the behavior we desire.
+      pushLocalDataToDatabase();
+    );
+    }
+});
+
+
+// Query the user for permission. Implement Periodic Background Sync
+const periodicSyncPermission = await navigator.permissions.query({
+  name: 'periodic-background-sync',
+});
+
+// Check if permission was properly granted.
+if (periodicSyncPermission.state == 'granted') {
+
+  // Register a new periodic sync.
+  await registration.periodicSync.register('fetch-new-content', {
+    // Set the sync to happen no more than once a day.
+    minInterval: 24 * 60 * 60 * 1000
+  });
+} 
+
+// Listen for the `periodicsync` event.
+self.addEventListener('periodicsync', event => {
+
+  // Check for correct tag on the periodicSyncPermissionsync event.
+  if (event.tag === 'fetch-new-content') {
+
+    // Execute the desired behavior with waitUntil().
+    event.waitUntil(
+
+      // This is just a hypothetical function for the behavior we desire.
+      fetchNewContent();
+    );
   }
 });
